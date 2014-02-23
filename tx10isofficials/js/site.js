46ad59a1cc2b4b25e7bf27da1c612f5d681a938e
Mike Flynn <mikeflynn4@gmail.com>
2014-02-22 23:30:53 -0600

    changes

--- tx10isofficials/js/site.js
+++ tx10isofficials/js/site.js
@@ -95,34 +95,31 @@ $(document).ready(function () {
 
 function formValidation() {
         var ref_email = $('input[name="ref_email"]');
-	var tournament = $('input[name="tounament"]');
+	var tournament = $('input[name="tournament"]');
 	var tid = $('input[name="tid"]');
-
-
-        var tournament = document.getElementById('tournament');
-        var tid = document.getElementById('tid');
-        var site = document.getElementById('site');
-        var name = document.getElementById('name');
-        var email = document.getElementById('email');
-        var phone = document.getElementById('phone');
-        var date = document.getElementById('date');
-        var time = document.getElementById('time');
-        var player = document.getElementById('playet');
-        var opponent  = document.getElementById('opponent');
-        var division  = document.getElementById('divisiion');
-        var code = document.getElementById('code');
-        var penalty = document.getElementById('penalty');
-        var court = document.getElementById('court');
-        var description = document.getElementById('description');
+	var site = $('input[name="site"]');
+	var oname = $('input[name="name"]');
+	var email = $('input[name="email"]');
+	var phone = $('input[name="phone"]');
+	var odate = $('input[name="date"]');
+	var oname = $('input[name="name"]');
+	var time = $('input[name="time"]');
+	var player = $('input[name="player"]');
+	var opponent = $('input[name="opponent"]');
+	var division = $('input[name="division"]');
+        var code = $('input[name="code"]');
+	var penalty = $('input[name="penalty"]');
+	var court = $('input[name="court"]');
+	var description = $('input[name="description"]');
 
         if(notEmpty(ref_email, "Please enter the referee's email address")){
           if(notEmpty(tournament, "Please enter the tournament name")){
             if(notEmpty(tid,"Please enter the tournament id")){
              if(notEmpty(site,"Please enter the site name")){
-               if(notEmpty(name,"Please enter your name")){
+               if(notEmpty(oname,"Please enter your name")){
                  if(notEmpty(email,"Please enter your email address")){
                    if(notEmpty(phone,"Please enter your phone number")){
-                     if(notEmpty(date,"Please enter the date of the offense")){
+                     if(notEmpty(odate,"Please enter the date of the offense")){
                        if(notEmpty(time,"Please enter the time of the offense")){
                          if(notEmpty(player,"Please enter the name of the player committing the offense")){
                            if(notEmpty(opponent,"Please enter the name of the opposing plaer")){
@@ -131,6 +128,7 @@ function formValidation() {
                                  if(notEmpty(penalty,"Please enter the penalty")){
                                    if(notEmpty(court,"Please enter the court number")){
                                      if(notEmpty(description,"Please enter the description of the offense")){
+					$('input[name="violation"]').submit();
                                        return true;{
                                      }
                                    }
@@ -153,12 +151,12 @@ function formValidation() {
 }
 
 function notEmpty(elem, helperMsg){
-console.log(elem.val());
+console.log("name: " +elem.attr('name'));
         if(elem.val().length < 0 || elem.val() == null || elem.val() == ''){
                 alert(helperMsg);
-                elem.focus();
                 return false;
-        }
-        //document.violation.submit.submit();
+        }else{
+		return true;
+	}
 }
 
