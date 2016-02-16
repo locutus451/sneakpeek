6fc0ec93bee6dfa059fc74525ae6a265981e3c8a
Mike Flynn <mikeflynn4@gmail.com>
2016-02-16 16:32:27 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+include 'header.html';
+include 'ext_functions.php';
+
+?>
+<br>
+<div class="wrapper">
+	<?php
+		include 'left_menu.html';
+	?>
+	<form name="register" action="register.php" method="post">
+                <div class="center">
+Please watch for emails from the National Office regarding schools.  The online tests should be avaiable soon.  There may be some additional schools in the Texas Area, keep a watch on your emails for those schools.
+                </div> 
+	</form>
+	<br>
+</div>
+<br>
+<br>
+<div class="clear">
+</div>
+<?php
+include 'footer.html';
+?>
+</body>
+</html>
