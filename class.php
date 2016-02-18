664bbeae7c1e81744f2e34c72af1428e20e521f1
Mike Flynn <mikeflynn4@gmail.com>
2016-02-17 19:08:23 -0600

    updated class.php

--- class.php
+++ class.php
@@ -10,8 +10,10 @@ include 'header.html';
                 <div class="center">
 		<br>
 		<form name="register" action="register.php" method="post"> 
-		Register for the Austin officials class for 2011.  Date: June 25th.<br><br>
+		Create your sneakpeek profile<br><br>
 		<font face="courier new">
+		Username: &nbsp&nbsp<input type="text" name="username" /><br><br>
+		Password: &nbsp&nbsp<input type="text" name="password" /><br><br>
 		First Name: <input type="text" name="first" /><br><br>
 		Last Name: &nbsp<input type="text" name="last" /><br><br>
 		Email:      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp<input type="text" name="email" /><br><br>
@@ -23,12 +25,6 @@ include 'header.html';
 		Zip:        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp<input type="text" name="zip" /><br><br>
 		</font>
 		<font name="verdana">
-		Please check the tests you are planning on taking:<br><br>
-		
-		<input type="checkbox" name="USTA_new"/>USTA new<br>
-                <input type="checkbox" name="USTA_new"/>USTA less than 3 years experience<br>
-                <input type="checkbox" name="ITA"/>ITA<br>
-                <input type="checkbox" name="Referee"/>Referee<br><br>
 		<input type=submit value="Register"/>
 		</font>
 		</form>
