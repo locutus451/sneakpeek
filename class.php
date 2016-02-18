33c58c90e9aa62b358a83cefebd9aa4748b20b8e
Mike Flynn <mikeflynn4@gmail.com>
2016-02-17 19:10:40 -0600

    updated class.php

--- class.php
+++ class.php
@@ -12,17 +12,17 @@ include 'header.html';
 		<form name="register" action="register.php" method="post"> 
 		Create your sneakpeek profile<br><br>
 		<font face="courier new">
-		Username: &nbsp&nbsp<input type="text" name="username" /><br><br>
-		Password: &nbsp&nbsp<input type="text" name="password" /><br><br>
-		First Name: <input type="text" name="first" /><br><br>
-		Last Name: &nbsp<input type="text" name="last" /><br><br>
-		Email:      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp<input type="text" name="email" /><br><br>
+		Username<font color=red>*</font>: &nbsp&nbsp<input type="text" name="username" /><br><br>
+		Password<font color=red>*</font>: &nbsp&nbsp<input type="text" name="password" /><br><br>
+		First Name<font color=red>*</font>: <input type="text" name="first" /><br><br>
+		Last Name<font color=red>*</font>: &nbsp<input type="text" name="last" /><br><br>
+		Email<font color=red>*</font>:      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp<input type="text" name="email" /><br><br>
 		Phone:      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp<input type="text" name="phone" /><br><br>
 		Address:    &nbsp;&nbsp;&nbsp<input type="text" name="street1" /><br><br>
 		Line 2:     &nbsp;&nbsp;&nbsp;&nbsp<input type="text" name="street2" /><br><br>
-		City:       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp<input type="text" name="city" /><br><br>
-		State:      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp<input type="text" name="state" maxlength=2 /><br><br>
-		Zip:        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp<input type="text" name="zip" /><br><br>
+		City<font color=red>*</font>:       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp<input type="text" name="city" /><br><br>
+		State<font color=red>*</font>:      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp<input type="text" name="state" maxlength=2 /><br><br>
+		Zip<font color=red>*</font>:        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp<input type="text" name="zip" /><br><br>
 		</font>
 		<font name="verdana">
 		<input type=submit value="Register"/>
